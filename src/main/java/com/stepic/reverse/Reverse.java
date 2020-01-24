58e849fd1711e449ad8fb9d78ba6d0ae6f437f59
mdzhezhelo <RhenjqGfhjkm2013>
2020-01-24 22:06:20 +0200

    made reverse;

--- src/main/java/com/stepic/reverse/Reverse.java
+++ src/main/java/com/stepic/reverse/Reverse.java
@@ -12,6 +12,8 @@ public class Reverse {
         final int[] arrayOdd = {1, 2, 3, 4, 5, 6, 7, 8, 9};
         final int[] reversedOdd = reverseOdd(arrayOdd);
         System.out.println(Arrays.toString(reversedOdd));
+        final int[] reversedEven = reverseEven(array);
+        System.out.println(Arrays.toString(reversedEven));
     }
 
     private static int[] reverseSimple(final int[] array) {
@@ -33,6 +35,25 @@ public class Reverse {
         if(length % 2 == 0) throw new RuntimeException();
 
         for (int i = length - 1, j = 0; i >= 0; i--, j++) {
+            final int tailValue = array[i];
+            final int firstValue = array[j];
+
+            if (tailValue == firstValue) break;
+
+            array[j] = tailValue;
+            array[i] = firstValue;
+        }
+
+        return array;
+    }
+
+    private static int[] reverseEven(final int[] array) {
+        if (array == null) throw new RuntimeException();
+
+        final int length = array.length;
+        if(length % 2 != 0) throw new RuntimeException();
+
+        for (int i = length - 1, j = 0; i >= (length - 1)/2; i--, j++) {
             final int tailValue = array[i];
             final int firstValue = array[j];
 
