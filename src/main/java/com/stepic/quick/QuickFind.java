b248db8b7137327b2a352491cc7b1e13296ea2af
mdzhezhelo <RhenjqGfhjkm2013>
2020-02-05 15:52:08 +0200

    made QuickFind and QuickUnion algorithms;

---
+++ src/main/java/com/stepic/quick/QuickFind.java
@@ -0,0 +1,24 @@
+package com.stepic.quick;
+
+public class QuickFind {
+    private int[] id;
+
+    public QuickFind(final int N) {
+        id = new int[N];
+        for (int i = 0; i < N; i++)  // N array accesses
+            id[i] = i;
+    }
+
+    public boolean connected(final int p, final int q) { //check whether p and q are in the same component (2 array accesses)
+        return id[p] == id[q];
+    }
+
+    public void union(int p, int q) {  //change all entries with id[p] to id[q] (at most 2N + 2 array accesses)
+        int pid = id[p];
+        int qid = id[q];
+        for (int i = 0; i < id.length; i++)
+            if (id[i] == pid) id[i] = qid;
+    }
+
+    // It takes N 2 array accesses to process a sequence of N union commands on N objects.
+}
---
+++ src/main/java/com/stepic/quick/QuickUnion.java
@@ -0,0 +1,28 @@
+package com.stepic.quick;
+
+public class QuickUnion {
+
+    private int[] id;
+
+    public QuickUnion(final int N) {
+        id = new int[N];
+        for (int i = 0; i < N; i++)
+            id[i] = i;
+    }
+
+    private int root(int i) { // chase parent pointers until reach root(depth of i array accesses)
+        while (i != id[i]) i = id[i];
+        return i;
+    }
+
+    public boolean connected(final int p, final int q) {
+        return root(p) == root(q);
+    }
+
+    public void union(final int p, final int q) {
+        int i = root(p);
+        int j = root(q);
+        id[i] = j;
+    }
+
+}
