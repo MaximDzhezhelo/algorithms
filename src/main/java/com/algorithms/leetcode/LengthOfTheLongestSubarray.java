68d77be948dc10ccaeea96043c23e745ef91c9e2
mdzhezhelo <mdzhezhelo@gmail.com>
2025-07-28 11:37:07 +0200

    LengthOfTheLongestSubarray by sum and contained symbol

--- src/main/java/com/algorithms/leetcode/LengthOfTheLongestSubarray.java
+++ src/main/java/com/algorithms/leetcode/LengthOfTheLongestSubarray.java
@@ -1,11 +1,11 @@
 package com.algorithms.leetcode;
 
-/**
- * Given an array of positive integers nums and an integer k, find the length of the longest subarray whose sum is less than or equal to k.
- */
 public class LengthOfTheLongestSubarray {
 
-    public int lengthOfTheLongestSubarray(int[] nums, int k) {
+    /**
+     * Given an array of positive integers nums and an integer k, find the length of the longest subarray whose sum is less than or equal to k.
+     */
+    public int bySum(int[] nums, int k) {
         if (nums == null || nums.length == 0) {
             return 0;
         }
@@ -25,6 +25,40 @@ public class LengthOfTheLongestSubarray {
                 leftB++;
             }
             answer = Math.max(answer, rightB - leftB + 1);
+        }
+
+        return answer;
+    }
+
+    /**
+     * You are given a binary string s (a string containing only "0" and "1"). You may choose up to one "0" and flip it to a "1". What is the length of the longest substring achievable that contains only "1"?
+     */
+    public int byContainedSymbol(String arr, int k) {
+        if (arr == null || arr.isEmpty()) {
+            return 0;
+        }
+
+        if (k == 0) {
+            return 0;
+        }
+
+        int left = 0;
+        int curr = 0;
+        int answer = 0;
+
+        for (int right = 0; right < arr.length(); right++) {
+            if (arr.charAt(right) == '0') {
+                curr++;
+            }
+
+            while (curr > k) {
+                if (arr.charAt(left) == '0') {
+                    curr--;
+                }
+                left++;
+            }
+
+            answer = Math.max(answer, right - left + 1);
         }
 
         return answer;
--- src/test/java/com/algorithms/leetcode/LengthOfTheLongestSubarrayTest.java
+++ src/test/java/com/algorithms/leetcode/LengthOfTheLongestSubarrayTest.java
@@ -7,14 +7,19 @@ import static org.junit.Assert.assertEquals;
 public class LengthOfTheLongestSubarrayTest {
 
     @Test
-    public void find() {
-        LengthOfTheLongestSubarray longestSubarray = new LengthOfTheLongestSubarray();
+    public void findBySum() {
+        var longestSubarray = new LengthOfTheLongestSubarray();
 
         int[] nums = {3, 1, 2, 7, 4, 2, 1, 1, 5};
-        assertEquals(4, longestSubarray.lengthOfTheLongestSubarray(nums, 8));
+        assertEquals(4, longestSubarray.bySum(nums, 8));
+    }
 
-//        int[] nums2 = {5};
-//        assertEquals(5.00000, maxAverage.findMaxAverageMy(nums2, 1), 4);
+    @Test
+    public void findByContainedSymbol() {
+        var longestSubarray = new LengthOfTheLongestSubarray();
+
+        var arr = "1101100111";
+        assertEquals(5, longestSubarray.byContainedSymbol(arr, 1));
     }
 
 }
